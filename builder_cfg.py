125089e8e68d9b7a6c400b3f5db62ca2fc9c7232
BorisBug <69357420+BorisBug@users.noreply.github.com>
2021-12-26 13:50:04 +0100

    001 build: keep folder folder structure & add cfg file

---
+++ builder_cfg.py
@@ -0,0 +1,10 @@
+# configuration for the building process
+
+# flags for gcc -> compile process
+cFlags = "-Wall -Werror -Wpedantic -Wextra"
+
+# flags for gcc -> linking process
+lFlags = ""
+
+# when building, the resulting files duplicate the same folder structure
+buildKeepFolderStructure = True
---
+++ sample/lib/buffer/buffer.h
@@ -0,0 +1,8 @@
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+
+#include <stdint.h>
+
+void buffer_set(uint8_t *buf, uint32_t size, uint8_t byte);
+
+#endif/*_BUFFER_H_*/
