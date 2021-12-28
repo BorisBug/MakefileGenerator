b8c908c9847b08860370bf1019db24b8ac139475
BorisBug <69357420+BorisBug@users.noreply.github.com>
2021-12-28 02:58:24 +0100

    006 sample: added more complex structure

--- sample/main.c
+++ sample/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "memset.h"
+#include "canbus.h"
 
 #define SIZE 5
 
 int main(void)
 {
     uint8_t array[SIZE];
-    printf("this is a test using 'buffer.h'\n");
+    printf("This is a test using 'memset.h' and 'canbus.h'\n");
     mem_set(array, SIZE, 0);
+    canbus_set_humidity(50);
     return 0;
 }
