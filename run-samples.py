0af67198e614bfb7a434b974b5592b67ea384617
BorisBug <69357420+BorisBug@users.noreply.github.com>
2022-01-03 20:39:51 +0100

    017 enable file name duplicated (only for 'keep_folder_structure = yes')

--- run-samples.py
+++ run-samples.py
@@ -1,50 +1,64 @@
 import os
 from maker import Maker
 
-# mkr = Maker()
-# mkr.load("samples/sample-multi-exe")
-# print("Files to convert into objects:")
-# mkr.printListSources()
-# print("Files becoming executables:")
-# mkr.printListExecs()
-# print("Folders with header files to include:")
-# mkr.printListIncludeFolders()
-# print("Dependencies for each file:")
-# mkr.printListDependencies()
-# mkr.build("sample/build", run=False, clean=True)
-# mkr.createMakefile("samples/sample-multi-exe/build")
-
-samples = [
-"sample-multi-exe",
-"sample-no-exe",
-"sample-one-exe",
-"sample-one-exe-complex"]
-
-try:
-
-    # the maker object
-    mkr = Maker()
-
-    for i, sample in enumerate(samples):
-        print("*"*50)
-        print(f"{i+1}. run: {sample}")
-        source = f"samples/{sample}"
-        build = f"{source}/build"
-
+def runOneSample():
+    try:
+        sampleDir = "samples/sample-multi-exe"
+        buildDir = sampleDir + "/build"
+        
+        mkr = Maker()
         # load source
-        mkr.load(source)
-
+        mkr.load(sampleDir)
+        print("Files to convert into objects:")
+        mkr.printListSources()
+        print("Files becoming executables:")
+        mkr.printListExecs()
+        print("Folders with header files to include:")
+        mkr.printListIncludeFolders()
+        print("Dependencies for each file:")
+        mkr.printListDependencies()
+        # compile
+        mkr.build(buildDir, run=False, clean=True)
         # create makefile
-        mkr.createMakefile(build)
+        mkr.createMakefile(buildDir)
 
-        # ..or build without the need of a makefile
-        #mkr.build(build)
+    except Exception as ex:
+        print(ex)
 
-        # run make with that makefile
-        cmd = f"cd {source} && make clean && make"
-        print(cmd)
-        os.system(cmd)
+def runAllSamples():
+    samples = [
+    "sample-multi-exe",
+    "sample-no-exe",
+    "sample-one-exe",
+    "sample-one-exe-complex"]
 
-except Exception as ex:
-    print(ex)
-    exit(1)
+    try:
+        # the maker object
+        mkr = Maker()
+
+        for i, sample in enumerate(samples):
+            print("*"*50)
+            print(f"{i+1}. run: {sample}")
+            sourceDir = f"samples/{sample}"
+            buildDir = f"{sourceDir}/build"
+
+            # load source
+            mkr.load(sourceDir)
+
+            # create makefile
+            mkr.createMakefile(buildDir)
+
+            # ..or build without the need of a makefile
+            #mkr.build(build)
+
+            # run make with that makefile
+            cmd = f"cd {sourceDir} && make clean && make"
+            print(cmd)
+            os.system(cmd)
+
+    except Exception as ex:
+        print(ex)
+
+
+runAllSamples()
+#runOneSample()
